0ff7e74a9c2d4f7a2970d454af46fad626fea23c
ackhalab <achraf.khalabi@gmail.com>
2025-05-03 20:18:49 +0400

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -1,12 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ackhalab <ackhalab@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025-05-03 15:12:34 by ackhalab          #+#    #+#             */
+/*   Updated: 2025-05-03 15:12:34 by ackhalab         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 32
-#endif
-# include <stdlib.h>
+# endif
 # include <fcntl.h>
 # include <stddef.h>
 # include <stdio.h>
+# include <stdlib.h>
 # include <unistd.h>
 
 char	*get_next_line(int fd);
@@ -15,7 +27,7 @@ char	*ft_strchr(char *s, int c);
 size_t	ft_strlen(char *s);
 char	*new_line(char *line);
 char	*ft_get_line(int fd, char *line);
-char	*ft_get_next_line(char	*line);
+char	*ft_get_next_line(char *line);
 void	ft_free(char **p);
 
 #endif
